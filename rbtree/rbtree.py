11eb0ae6f4111d792f8ea0ca1f5cc609faa8b862
canvas <canvas@canvasdeMacBook-Pro.local>
2019-06-15 08:03:28 +0800

    红黑树

--- rbtree/rbtree.py
+++ rbtree/rbtree.py
@@ -1,4 +1,4 @@
-#红黑树其实是2 3树的表示
+# 红黑树其实是2 3树的表示
 # 红色节点表示 和黑色平级 在左边
 # 节点初始化的时候是红色,表示希望去融合新节点和 2 3树一样
 # 2 3树中不会把节点加在空节点里面 而是去融合
@@ -8,3 +8,20 @@
 #
 #
 #
+
+RED = True
+BLACK = False
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        # 初始化的时候,把颜色指定为红色,红色表示去融合,2 3树的所有节点都是先融合再变形
+        self.color = RED
+
+
+class RBTree:
+    def __init__(self):
+        pass
